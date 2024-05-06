5a86c1b8109d6eae63b84edcc6e4c1a21693f187
JuanchoZGZ17 <756640@unizar.es>
2024-05-06 22:50:27 +0200

    Mis reservas + About -> OK pendiente de backend

--- src/ui/pages/AboutPage.tsx
+++ src/ui/pages/AboutPage.tsx
@@ -3,8 +3,80 @@ import React from 'react';
 const AboutPage: React.FC = () => {
   return (
     <div>
-      <h2>Aboute Page</h2>
-      {/* Contenido de la página */}
+      <div className="mt-4">
+        <h3 className="text-lg font-bold mb-2">Reglas para realizar reservas:</h3>
+        <table className="w-full border-collapse border border-gray-400">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-400 px-4 py-2">Roles \ Tipos</th>
+              <th className="border border-gray-400 px-4 py-2">Aula</th>
+              <th className="border border-gray-400 px-4 py-2">Seminario</th>
+              <th className="border border-gray-400 px-4 py-2">Laboratorio</th>
+              <th className="border border-gray-400 px-4 py-2">Despacho</th>
+              <th className="border border-gray-400 px-4 py-2">Sala Común</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Estudiante</td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Investigador contratado</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Docente-investigador</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Conserje</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Técnico de laboratorio</td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2">Sí *</td>
+              <td className="border border-gray-400 px-4 py-2"></td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Gerente</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+            <tr>
+              <td className="border border-gray-400 px-4 py-2">Gerente-docente-investigador</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+              <td className="border border-gray-400 px-4 py-2">Sí</td>
+            </tr>
+          </tbody>
+        </table>
+        <p className="mt-2 text-sm italic">* Los técnicos de laboratorio, investigadores contratados y docentes-investigadores solo pueden reservar laboratorios y seminarios que estén adscritos a su mismo departamento o la EINA.</p>
+      </div>
     </div>
   );
 };
--- src/ui/pages/MyReservationPage.tsx
+++ src/ui/pages/MyReservationPage.tsx
@@ -1,13 +1,140 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ISpaceRepo, Space } from '../../core/space/domain';
+import { MemorieSpaceRepo } from '../../infraestructure/memory/SpaceRepo';
+import { MainLayout } from '../components/MainLayout';
+import { useEffect, useState } from 'react';
+import { SpaceList } from '../components/SpaceList';
+import { Mapa } from '../components/Mapa';
+import { HttpSpaceRepo } from '../../infraestructure/http/SpaceRepo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyReservationPage: React.FC = () => {
-  // Contenido de la página
-  return (
-    <div>
-      <h2>My Reservation Page</h2>
-      {/* Contenido de la página */}
-    </div>
-  );
+    // Definir la lista de espacios de prueba
+    const list: Space[] = [
+      {
+        name: 'Despacho 1',
+        id: '1',
+        address: 'Planta 1',
+        kind: 'despacho',
+        price: 240000,
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Laboratorio 2',
+        id: '2',
+        address: 'Planta 2',
+        kind: 'laboratorio',
+        price: 240000,
+        owner: 'Juan',
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Sala común 1',
+        id: '3',
+        address: 'Planta 1',
+        kind: 'salacomun',
+        price: 240000,
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Seminario 4',
+        id: '4',
+        address: 'Planta 3',
+        kind: 'seminario',
+        price: 240000,
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Aula 1',
+        id: '1',
+        address: 'Planta 3',
+        kind: 'aula',
+        price: 240000,
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Aula 2',
+        id: '2',
+        address: 'Planta 3',
+        kind: 'aula',
+        price: 240000,
+        owner: 'Juan',
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Aula 3',
+        id: '3',
+        address: 'Planta 3',
+        kind: 'aula',
+        price: 240000,
+        lat: 41.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+      {
+        name: 'Aula 4',
+        id: '4',
+        address: 'Planta 1',
+        kind: 'aula',
+        price: 240000,
+        lat: 40.6488,
+        lng: -0.8891,
+        income: 0,
+      },
+    ];
+  
+    // Comentar la línea de espacioRepo y useEffect para usar la lista de prueba
+    // const spaceRepo: ISpaceRepo = new HttpSpaceRepo();
+    // const [spacesList, setSpacesList] = useState<Space[]>([]);
+  
+    // useEffect(() => {
+    //   spaceRepo
+    //     .getAllSpaces()
+    //     .then((list) => {
+    //       setSpacesList(list);
+    //     })
+    //     .catch((error) => {
+    //       toast.error('Error al obtener los edificios', {
+    //         position: 'top-right',
+    //         autoClose: 5000,
+    //         hideProgressBar: false,
+    //         closeOnClick: true,
+    //         pauseOnHover: true,
+    //         draggable: true,
+    //         progress: undefined,
+    //         theme: 'light',
+    //       });
+    //     });
+    // }, []);
+
+    return (
+      <MainLayout>
+        <ToastContainer />
+        <div className='w-full h-full flex justify-center items-center md:pb-40 px-6'>
+          <div className='h-full md:w-1/2 px-2 w-full '>
+            <h2 className="text-2xl font-bold mb-4 text-primary">Mis reservas</h2>
+            <SpaceList list={list} />
+          </div>
+          <div className='h-full md:w-1/2 px-2 w-full '>
+            <h2 className="text-2xl font-bold mb-4 text-primary">Reservas potencialmente inválidas</h2>
+            <SpaceList list={list} />
+          </div>
+        </div>
+      </MainLayout>
+    );
 };
 
 export default MyReservationPage; // Asegúrate de exportar el componente
