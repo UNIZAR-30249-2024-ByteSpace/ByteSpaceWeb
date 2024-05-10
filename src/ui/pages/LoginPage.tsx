65290efebb4ba04cc680578e924bdd5ff678d403
JuanchoZGZ17 <756640@unizar.es>
2024-05-10 22:30:33 +0200

    Login letra

--- src/ui/pages/LoginPage.tsx
+++ src/ui/pages/LoginPage.tsx
@@ -24,9 +24,9 @@ const LoginPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundImage: 'url(/src/assets/Fondo.svg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className='bg-secondary bg-opacity-80 p-8 rounded-lg w-96 h-80 flex flex-col justify-center items-center'>
-        <h1 className="text-3xl text-center font-semibold mb-4 text-primary">ByteSpace</h1>
+        <h1 className="text-3xl text-center font-semibold mb-4 text-primary text-xxxl">ByteSpace</h1>
         <div className="mb-4">
-          <label htmlFor="username" className="block text-primary">Correo electrónico</label>
+          <label htmlFor="username" className="block text-primary text-xl">Correo electrónico</label>
           <input
             type="text"
             id="username"
@@ -36,7 +36,7 @@ const LoginPage: React.FC = () => {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="password" className="block text-bold text-primary">Contraseña</label>
+          <label htmlFor="password" className="block text-bold text-primary text-xl">Contraseña</label>
           <input
             type="password"
             id="password"
@@ -46,7 +46,7 @@ const LoginPage: React.FC = () => {
           />
         </div>
         <button
-          className="w-full bg-blue-500 text-primary text-bold py-2 rounded-md hover:bg-blue-600 transition-colors"
+          className="w-full bg-blue-500 text-primary text-bold py-2 rounded-md hover:bg-blue-600 transition-colors text-xl"
           onClick={handleLogin}
         >
           Iniciar sesión
