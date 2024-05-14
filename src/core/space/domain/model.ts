cdb3741ed73bc4cceff3b76e8d862c9406322616
Rael167 <75581099+Rael167@users.noreply.github.com>
2024-05-14 19:44:45 +0200

    small changes

--- src/core/space/domain/model.ts
+++ src/core/space/domain/model.ts
@@ -3,11 +3,11 @@ export type Kind = 'aula' | 'salacomun' | 'seminario' | 'laboratorio' | 'despach
 export type Space = {
   id: string;
   tamanio: number;
-  kind: Kind; // Utiliza la enumeración Kind en lugar de un tipo string
+  tipo: string; 
   maxOcupantes: number;
   informacion: string;
   reservable: boolean;
-  categoria: string;
+  categoria: Kind; // Utiliza la enumeración Kind en lugar de un tipo string
   porcentajeOcupacion: number;
   planta: number;
   asignadoA: string;
