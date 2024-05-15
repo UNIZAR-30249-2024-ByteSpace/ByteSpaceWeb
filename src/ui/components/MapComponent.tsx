8ff42635a62fe005545f6ac35f45f34a16676d9e
JuanchoZGZ17 <756640@unizar.es>
2024-05-15 09:49:22 +0200

    Mapa con plantas fix

--- src/ui/components/MapComponent.tsx
+++ src/ui/components/MapComponent.tsx
@@ -1,71 +1,67 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { NavLink } from 'react-router-dom';
 import { Space } from '../../core/space/domain';
-
-type Sizes = 'small' | 'regular';
-
-interface Props {
-    list: Space[]; // Cambia 'Space' por 'any' si no se necesita la definición de 'Space'
-  }
 
 // Personalizar el icono predeterminado de Leaflet
 const blueIcon = L.icon({
     iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.6.0/images/marker-icon.png',
     iconSize: [38, 38],
     iconAnchor: [19, 38],
-  });
+});
 
-const MapComponent: React.FC = () => {
-  const mapContainer = useRef<HTMLDivElement>(null);
-  const [selectedPlanta, setSelectedPlanta] = useState<string>('planta0'); // Estado para la planta seleccionada
+interface MapComponentProps {
+    selectedFloor: number;
+    setSelectedFloor: (floor: number) => void;
+}
 
-  useEffect(() => {
-    if (!mapContainer.current) return;
+const MapComponent: React.FC<MapComponentProps> = ({ selectedFloor, setSelectedFloor }) => {
+    const mapContainer = useRef<HTMLDivElement>(null);
 
-    const map = L.map(mapContainer.current, {
-      center: [41.6835, -0.889],
-      zoom: 30
-    });
+    useEffect(() => {
+        if (!mapContainer.current) return;
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        const map = L.map(mapContainer.current, {
+            center: [41.6835, -0.889],
+            zoom: 30
+        });
 
-    const plantaIndex = selectedPlanta === 'planta0' ? 0 : selectedPlanta === 'planta1' ? 1 : selectedPlanta === 'planta2' ? 2 : selectedPlanta === 'planta3' ? 3 : 4;
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    L.tileLayer.wms("http://localhost:8080/geoserver/ProyectoLabis/wms", {
-      layers: `ProyectoLabis:ada_planta_${plantaIndex}`,
-      format: 'image/png',
-      transparent: true
-    }).addTo(map);
+        const plantaIndex = selectedFloor;
 
-    return () => {
-      map.remove();
+        L.tileLayer.wms("http://localhost:8080/geoserver/ProyectoLabis/wms", {
+            layers: `ProyectoLabis:ada_planta_${plantaIndex}`,
+            format: 'image/png',
+            transparent: true
+        }).addTo(map);
+
+        return () => {
+            map.remove();
+        };
+    }, [selectedFloor]);
+
+    const handlePlantaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedFloor(parseInt(e.target.value));
     };
-  }, [selectedPlanta]);
 
-  const handlePlantaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedPlanta(e.target.value);
-  };
+    return (
+        <div className='w-full overflow-x-clip pr-2 h-full text-primary font-bold text-xl'>
+            {/* Selector de plantas */}
+            <select value={selectedFloor} onChange={handlePlantaChange}>
+                <option value={0}>Planta 0</option>
+                <option value={1}>Planta 1</option>
+                <option value={2}>Planta 2</option>
+                <option value={3}>Planta 3</option>
+                <option value={4}>Planta 4</option>
+            </select>
 
-  return (
-    <div className='w-full overflow-x-clip pr-2 h-full text-primary font-bold text-xl'>
-      {/* Selector de plantas */}
-      <select value={selectedPlanta} onChange={handlePlantaChange}>
-        <option value="planta0">Planta 0</option>
-        <option value="planta1">Planta 1</option>
-        <option value="planta2">Planta 2</option>
-        <option value="planta3">Planta 3</option>
-        <option value="planta4">Planta 4</option>
-      </select>
-
-      {/* Mapa */}
-      <div ref={mapContainer} style={{ width: '960px', height: '500px' }} />
-    </div>
-  );
+            {/* Mapa */}
+            <div ref={mapContainer} style={{ width: '960px', height: '500px' }} />
+        </div>
+    );
 };
 
 export { MapComponent };
--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -9,46 +9,45 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const HomePage = () => {
-  
     const spaceRepo: ISpaceRepo = new HttpSpaceRepo();
     const [spacesList, setSpacesList] = useState<Space[]>([]);
+    const [selectedFloor, setSelectedFloor] = useState<number>(0); // Añadir estado para la planta seleccionada
   
     useEffect(() => {
-       spaceRepo
-         .getAllSpaces()
-         .then((list) => {
-           setSpacesList(list);
-         })
-         .catch((error) => {
-           toast.error('Error al obtener los edificios', {
-             position: 'top-right',
-             autoClose: 5000,
-             hideProgressBar: false,
-             closeOnClick: true,
-             pauseOnHover: true,
-             draggable: true,
-             progress: undefined,
-             theme: 'light',
-           });
-         });
-     }, []);
+        spaceRepo
+            .getAllSpaces()
+            .then((list) => {
+                setSpacesList(list);
+            })
+            .catch((error) => {
+                toast.error('Error al obtener los edificios', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'light',
+                });
+            });
+    }, []);
 
-     console.log('Lista de espacios:', spacesList); // Agregado: Verifica si spacesList tiene contenido
+    const filteredSpacesList = spacesList.filter(space => space.planta === selectedFloor); // Filtrar los espacios por planta seleccionada
 
-
-  return (
-    <MainLayout>
-      <ToastContainer />
-      <div className='w-full h-full flex justify-center items-center md:pb-40 px-6'>
-        <div className='h-full md:w-1/2 px-2 w-full '>
-          <SpaceList list={spacesList} />
-        </div>
-        <div className=' h-full w-1/2 px-2  collapse md:visible'>
-          <MapComponent/>
-        </div>
-      </div>
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <ToastContainer />
+            <div className='w-full h-full flex justify-center items-center md:pb-40 px-6'>
+                <div className='h-full md:w-1/2 px-2 w-full '>
+                    <SpaceList list={filteredSpacesList} /> {/* Usar la lista filtrada */}
+                </div>
+                <div className='h-full w-1/2 px-2 collapse md:visible'>
+                    <MapComponent selectedFloor={selectedFloor} setSelectedFloor={setSelectedFloor} /> {/* Pasar el estado y el actualizador */}
+                </div>
+            </div>
+        </MainLayout>
+    );
 };
 
 export default HomePage;
