744400ebd63a5a1e1e54eb3f2105c25e2f606010
JuanchoZGZ17 <756640@unizar.es>
2024-05-21 15:58:30 +0200

    RF18  La aplicación debe permitir a los gerentes consultar las reservas activas (hora de finalización posterior a la actual).

--- src/ui/pages/AdminPage.tsx
+++ src/ui/pages/AdminPage.tsx
@@ -7,27 +7,28 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuth } from '../components/AuthContext'; // Importa el hook useAuth
 import { useNavigate } from 'react-router-dom';
+import { MdOutlineSwitchLeft, MdOutlineSwitchRight } from "react-icons/md";
 
 const reserveRepo: IReserveRepo = new HttpReserveRepo();
-
 
 const AdminPage: React.FC = () => {
   const [reservesList, setReserveList] = useState<Reserve[]>([]);
   const { user } = useAuth(); // Obtén el ID del usuario del contexto de autenticación
   const [validReserves, setValidReserves] = useState<Reserve[]>([]);
   const [invalidReserves, setInvalidReserves] = useState<Reserve[]>([]);
+  const [activeReserves, setActiveReserves] = useState<Reserve[]>([]);
+
   const navigate = useNavigate();
+  const [switchPosition, setSwitchPosition] = useState<boolean>(true); // Estado para la posición del switch
 
   useEffect(() => {
     if (user) { // Verifica que haya un ID de usuario antes de hacer la solicitud
       reserveRepo
         .getAllReservesAdmin() // Utiliza el ID del usuario actual
         .then((list) => {
-          console.log('Received reserves:', list); // Añadir información de depuración
           setReserveList(list);
         })
         .catch((error) => {
-          console.log('Error fetching reserves:', error); // Añadir información de depuración
           toast.error('Error al obtener las reservas', {
             position: 'top-right',
             autoClose: 5000,
@@ -43,11 +44,30 @@ const AdminPage: React.FC = () => {
   }, [user]); // Asegúrate de ejecutar el efecto cuando cambie el ID
 
   useEffect(() => {
-    // Actualizar listas de reservas válidas e inválidas cuando reservesList cambie
+
+    const fechaHoraActual = new Date();
+    const horaActual = fechaHoraActual.getHours(); // Obtenemos la hora actual en formato de 24 horas
+    fechaHoraActual.setUTCHours(0, 0, 0, 0);
+    const fechaActualISOString = fechaHoraActual.toISOString();
+
     const updatedValidReserves = reservesList.filter(reserve => !reserve.potencialInvalida);
     const updatedInvalidReserves = reservesList.filter(reserve => reserve.potencialInvalida);
+
+    // Reservas activas
+    const updatedActiveReserves = reservesList.filter(reserve => {
+      const reserveFecha = new Date(reserve.fecha).toISOString().split('T')[0];
+      const fechaActual = fechaActualISOString.split('T')[0];
+      return (
+        reserveFecha === fechaActual &&
+        reserve.horaInicio <= horaActual &&
+        horaActual <= reserve.horaFin
+      );
+    });
+    
     setValidReserves(updatedValidReserves);
     setInvalidReserves(updatedInvalidReserves);
+    setActiveReserves(updatedActiveReserves);
+
   }, [reservesList]);
   
   const handleCancelReserve = async (id: string) => {
@@ -69,12 +89,10 @@ const AdminPage: React.FC = () => {
         reserveRepo
           .getAllReservesAdmin() // Utiliza el ID del usuario actual
           .then((list) => {
-            console.log('Received reserves:', list); // Añadir información de depuración
             // Actualizar la lista de reservas después de la cancelación
             setReserveList(list);
           })
           .catch((error) => {
-            console.log('Error fetching reserves:', error); // Añadir información de depuración
             toast.error('Error al obtener las reservas', {
               position: 'top-right',
               autoClose: 5000,
@@ -88,11 +106,28 @@ const AdminPage: React.FC = () => {
           });
       }
   
-      // Actualizar listas de reservas válidas e inválidas cuando reservesList cambie
-      const updatedValidReserves = reservesList.filter(reserve => !reserve.potencialInvalida);
-      const updatedInvalidReserves = reservesList.filter(reserve => reserve.potencialInvalida);
-      setValidReserves(updatedValidReserves);
-      setInvalidReserves(updatedInvalidReserves);
+      const fechaHoraActual = new Date();
+    const horaActual = fechaHoraActual.getHours(); // Obtenemos la hora actual en formato de 24 horas
+    fechaHoraActual.setUTCHours(0, 0, 0, 0);
+    const fechaActualISOString = fechaHoraActual.toISOString();
+
+    const updatedValidReserves = reservesList.filter(reserve => !reserve.potencialInvalida);
+    const updatedInvalidReserves = reservesList.filter(reserve => reserve.potencialInvalida);
+
+    // Reservas activas
+    const updatedActiveReserves = reservesList.filter(reserve => {
+      const reserveFecha = new Date(reserve.fecha).toISOString().split('T')[0];
+      const fechaActual = fechaActualISOString.split('T')[0];
+      return (
+        reserveFecha === fechaActual &&
+        reserve.horaInicio <= horaActual &&
+        horaActual <= reserve.horaFin
+      );
+    });
+    
+    setValidReserves(updatedValidReserves);
+    setInvalidReserves(updatedInvalidReserves);
+    setActiveReserves(updatedActiveReserves);
   
     } catch (error) {
       console.error('Error al cancelar la reserva:', error);
@@ -128,12 +163,10 @@ const AdminPage: React.FC = () => {
         reserveRepo
           .getAllReservesAdmin() // Utiliza el ID del usuario actual
           .then((list) => {
-            console.log('Received reserves:', list); // Añadir información de depuración
             // Actualizar la lista de reservas después de la cancelación
             setReserveList(list);
           })
           .catch((error) => {
-            console.log('Error fetching reserves:', error); // Añadir información de depuración
             toast.error('Error al obtener las reservas', {
               position: 'top-right',
               autoClose: 5000,
@@ -147,13 +180,29 @@ const AdminPage: React.FC = () => {
           });
       }
   
-      // Actualizar listas de reservas válidas e inválidas cuando reservesList cambie
-      const updatedValidReserves = reservesList.filter(reserve => !reserve.potencialInvalida);
-      const updatedInvalidReserves = reservesList.filter(reserve => reserve.potencialInvalida);
-      setValidReserves(updatedValidReserves);
-      setInvalidReserves(updatedInvalidReserves);
+      const fechaHoraActual = new Date();
+    const horaActual = fechaHoraActual.getHours(); // Obtenemos la hora actual en formato de 24 horas
+    fechaHoraActual.setUTCHours(0, 0, 0, 0);
+    const fechaActualISOString = fechaHoraActual.toISOString();
 
+    const updatedValidReserves = reservesList.filter(reserve => !reserve.potencialInvalida);
+    const updatedInvalidReserves = reservesList.filter(reserve => reserve.potencialInvalida);
 
+    // Reservas activas
+    const updatedActiveReserves = reservesList.filter(reserve => {
+      const reserveFecha = new Date(reserve.fecha).toISOString().split('T')[0];
+      const fechaActual = fechaActualISOString.split('T')[0];
+      return (
+        reserveFecha === fechaActual &&
+        reserve.horaInicio <= horaActual &&
+        horaActual <= reserve.horaFin
+      );
+    });
+    
+    setValidReserves(updatedValidReserves);
+    setInvalidReserves(updatedInvalidReserves);
+    setActiveReserves(updatedActiveReserves);
+      
     } catch (error) {
       console.error('Error al aceptar la reserva:', error);
       toast.error('Error al aceptar la reserva', {
@@ -169,15 +218,28 @@ const AdminPage: React.FC = () => {
     }
   };
 
+  const handleSwitchToggle = () => {
+    setSwitchPosition(!switchPosition); // Cambiar la posición del switch
+  };
+
   return (
     <MainLayout>
       <ToastContainer />
       <div className='w-full h-full flex justify-center items-center md:pb-40 px-6'>
+        {switchPosition ? ( // Mostrar DIV 1 si switchPosition es true
         <div className='h-full md:w-1/2 px-2 w-full '>
-          <h2 className="text-2xl font-bold mb-4 text-primary">Reservas</h2>
-          <ReserveList list={validReserves} onCancel={handleCancelReserve} onAccept={handleAcceptReserve} />
-        </div>
-        <div className='h-full md:w-1/2 px-2 w-full '>
+          <span className="text-2xl font-bold mb-4 text-primary">Reservas</span>
+            <span className='text-2xl font-bold mb-4 ml-40 text-primary'><button onClick={handleSwitchToggle}><MdOutlineSwitchRight /> {switchPosition}</button></span>
+              <ReserveList list={validReserves} onCancel={handleCancelReserve} onAccept={handleAcceptReserve} />
+          </div>
+          ) : ( // Mostrar DIV 2 si switchPosition es false
+          <div className='h-full md:w-1/2 px-2 w-full'>
+          <span className="text-2xl font-bold mb-4 text-primary">Reservas activas</span>
+              <span className='text-2xl font-bold mb-4 ml-20 text-primary'><button onClick={handleSwitchToggle}><MdOutlineSwitchLeft /> {switchPosition}</button></span>
+              <ReserveList list={activeReserves} onCancel={handleCancelReserve} onAccept={handleAcceptReserve} />
+            </div>
+          )}
+        <div className='h-full md:w-1/2 px-2 w-full'>
           <h2 className="text-2xl font-bold mb-4 text-primary">Reservas potencialmente inválidas</h2>
           <ReserveList list={invalidReserves} onCancel={handleCancelReserve} onAccept={handleAcceptReserve} />
         </div>
